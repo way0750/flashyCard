24d586011a6150c2e3a57e6656bb9a589f9ade72
way0750 <bruce0750@gmail.com>
2015-11-09 23:17:17 -0800

    http request for files returns correct data

--- main/server/server.js
+++ main/server/server.js
@@ -16,8 +16,11 @@ app.get('/hey', function(req, res){
   res.sendStatus(200);
 });
 
-app.get('/readFile', function(req, res, next){
+
+//send one file to client
+app.post('/readFile', function(req, res, next){
   var fileName = req.body.fileName;
+  console.log('/readFile', req.body.fileName, flashyDir+fileName);
   fs.readFile(flashyDir+fileName, 'utf8', function(err, data){
     if (err){
       res.sendStatus(400);
@@ -27,6 +30,7 @@ app.get('/readFile', function(req, res, next){
   });//close fs
 });
 
+//read all files, and only return the names
 app.get('/listFiles', function(req, res, next){
   fs.readdir(flashyDir, function(err, data){
     if (err){
