5d6ae158de9dc83a51b1a8094ec2a1a699ed4267
way0750 <bruce0750@gmail.com>
2015-11-09 22:05:08 -0800

    initial commit, implemented basic structure of the application http request for files returns correct data global data object to share persist data

---
+++ main/client/fileList/fileList.js
@@ -0,0 +1,27 @@
+var fileList = angular.module('fileList', []);
+
+fileList.controller('fileListCtrl', function ($scope, $http, getFileList, $location) {
+
+  $scope.getFiles = function(){
+    getFileList.getFiles().then(function(res){
+      $scope.fileList = res.data;
+    });
+  };
+
+  //every time this view is loaded, file list should be updated as well;
+  $scope.getFiles();
+
+  //this function should do two things get file by $http save it in the client.curStack
+  //then 
+  $scope.getFile = function(){
+    getFileList.dataObj.curStackName = $scope.fileName;
+    getFileList.getStack($scope.fileName)
+      .then(function(data){
+        getFileList.dataObj.curStack = data;
+      })
+      .then(function(){
+        $location.path( '/study' );
+      });
+  };//close getFile
+
+});
---
+++ main/server/server.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+
+var flashyDir = '../../flashy cards/';
+
+var port = 8888;
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.use(express.static('../client'));
+
+app.get('/hey', function(req, res){
+  res.sendStatus(200);
+});
+
+
+//send one file to client
+app.post('/readFile', function(req, res, next){
+  var fileName = req.body.fileName;
+  console.log('/readFile', req.body.fileName, flashyDir+fileName);
+  fs.readFile(flashyDir+fileName, 'utf8', function(err, data){
+    if (err){
+      res.sendStatus(400);
+    } else {
+      res.send(data);
+    }//close if statement
+  });//close fs
+});
+
+//read all files, and only return the names
+app.get('/listFiles', function(req, res, next){
+  fs.readdir(flashyDir, function(err, data){
+    if (err){
+      res.sendStatus(400, 'something terrible happened');
+    } else {
+      res.send(data);
+    }//close if statement
+  }); //close fs
+});
+
+app.post('/writeFile', function(req, res, next){
+  var fileName = req.body.fileName;
+  fs.writeFile(flashyDir+fileName, req.body.file, function(err){
+    if (err){
+      res.sendStatus(500);
+    } else {
+      res.sendStatus(200, "got the file");
+    }
+
+  });//close fs
+
+});
+
+app.listen(port);
+
+console.log('this should be running at http://localhost:'+port);
